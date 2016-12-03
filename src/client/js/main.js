ce19ba977c60a28e0f026b5b704b742cac919101
Tim Rottmann <tim.rottmann@gmail.com>
2016-12-03 13:31:46 +0100

    wip

---
+++ src/client/js/main.js
@@ -0,0 +1,10 @@
+import io from 'socket.io'
+
+let socket = io('http://localhost:1337');
+
+socket.on('connection', function () {
+    console.log("piahdfoidfosn");
+});
+socket.on('message', msg => {
+    console.log('hello', msg);
+});
